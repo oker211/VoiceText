1d3a4193751175ac323bf3b9327f8108fa25bb47
Filipp Slabodetsky <oker211@gmail.com>
2020-05-20 12:02:54 +0300

    Initial Commit

---
+++ VoiceText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceText
+//
+//  Created by Филипп Слабодецкий on 20.05.2020.
+//  Copyright © 2020 Filipp Slabodetsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
